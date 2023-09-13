02a64488e8ffe177b815e68902710036e83b0338
Son-2k <sonnnfx21638@funix.edu.vn>
2023-09-13 16:03:58 +0700

    Updates

---
+++ precache-manifest.bf60282055931bbcfaea49a351b77f82.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7def4987d5a212a0501",
+    "url": "/ass_3_react/static/css/main.37a2cff4.chunk.css"
+  },
+  {
+    "revision": "c7def4987d5a212a0501",
+    "url": "/ass_3_react/static/js/main.193bbcd6.chunk.js"
+  },
+  {
+    "revision": "2be2aaa64a8c63519a62",
+    "url": "/ass_3_react/static/js/runtime~main.8b05e144.js"
+  },
+  {
+    "revision": "0675fe1944ba69bb8987",
+    "url": "/ass_3_react/static/css/2.5afb9c60.chunk.css"
+  },
+  {
+    "revision": "0675fe1944ba69bb8987",
+    "url": "/ass_3_react/static/js/2.9471eefd.chunk.js"
+  },
+  {
+    "revision": "93b7003b8b3fe8bac328",
+    "url": "/ass_3_react/static/js/3.c17d3b5c.chunk.js"
+  },
+  {
+    "revision": "67addf5b6a78eee6c62fc0934e9b4fb5",
+    "url": "/ass_3_react/static/media/caret-left-solid.67addf5b.svg"
+  },
+  {
+    "revision": "fd82da797f1b6f703b0931d0b8d0b3c0",
+    "url": "/ass_3_react/static/media/caret-right-solid.fd82da79.svg"
+  },
+  {
+    "revision": "3ea3fe2d4d3ee073b5bca4bfeaa0620a",
+    "url": "/ass_3_react/static/media/banner1.3ea3fe2d.jpg"
+  },
+  {
+    "revision": "2226e8c5b8bb36e71e85efe6ca56dcd3",
+    "url": "/ass_3_react/static/media/product_1.2226e8c5.png"
+  },
+  {
+    "revision": "050782eff4f52d2e237ef92db54ee439",
+    "url": "/ass_3_react/static/media/product_2.050782ef.png"
+  },
+  {
+    "revision": "e852e55d7a1a1c19cf380ab7db6bcc74",
+    "url": "/ass_3_react/static/media/product_3.e852e55d.png"
+  },
+  {
+    "revision": "261d3324f210f9e413a6e30a8adb48c4",
+    "url": "/ass_3_react/static/media/product_4.261d3324.png"
+  },
+  {
+    "revision": "d5c0e34a9663f1b4e9fa8807f60bc108",
+    "url": "/ass_3_react/static/media/product_5.d5c0e34a.png"
+  },
+  {
+    "revision": "a6744c1a42b0c28ec712a5515a4c2c84",
+    "url": "/ass_3_react/index.html"
+  }
+];
